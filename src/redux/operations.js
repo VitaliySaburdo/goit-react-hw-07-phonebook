3d6a6d72d1a25530e6c9a7718c45f4f4b2e22983
VitaliySaburdo <vsaburdo@gmail.com>
2023-02-13 19:20:37 +0200

    3

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
+axios.defaults.baseURL = 'https://63ea64d73363c8700368743e.mockapi.io';
 const fetchContacts = () => async dispatch => {
   try {
     const response = await axios.get('/contacts');
