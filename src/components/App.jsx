3e69bd32e563cdc4aa1ca582754155ea4ab0d8bc
VitaliySaburdo <vsaburdo@gmail.com>
2023-02-13 19:39:51 +0200

    8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,29 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { Section } from './Section/Section';
-import { RenderContacts } from './RenderContactsList/RenderContactsList';
 import Filter from './Filter/Filter';
+import { RenderContacts } from './RenderContactsList/RenderContactsList';
 import PropTypes from 'prop-types';
 import { Container } from './App.styled';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "redux/operations";
+import { getContacts } from "redux/selectors";
 
 export function App() {
+
+    const dispatch = useDispatch();
+  // Получаем части состояния
+  const { items, isLoading, error } = useSelector(getContacts);
+  // Вызываем операцию
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <Container>
+      {isLoading && <p>Loading contacts...</p>}
+      {error && <p>{error}</p>}
+      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
       <Section title="Phonebook" />
       <ContactForm  />
       <Section title="Contacts" />
